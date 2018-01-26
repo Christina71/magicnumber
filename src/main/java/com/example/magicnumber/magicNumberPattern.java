a2a2eb9a198f6f5817b6799ec3a47706e4e8139a
Christina71 <mariachristina71@yahoo.com>
2018-01-26 13:52:33 -0500

    Initial commit for magic number

---
+++ src/main/java/com/example/magicnumber/magicNumberPattern.java
@@ -0,0 +1,35 @@
+package com.example.magicnumber;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+@RestController
+public class magicNumberPattern {
+
+    @RequestMapping("/magic")
+
+    public String magicNumber(@RequestParam("number") int num) {
+        String output = " ";
+        Scanner input = new Scanner(System.in);
+
+
+        while (num!=1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+
+                output = output + " " + num  ;
+            } else {
+                num = num * 3 + 1;
+
+                output =  output + " " + num ;
+            }
+
+        }
+        return "Your web page prints:" + "<br>" + "Starting number: "  + num +"<br>" + output;
+    }
+}
+
+
